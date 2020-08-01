d952348371949112ca4d7b78d63bd374b0a34e82
NaI <fd4124983399@gmail.com>
2020-08-02 00:48:56 +0800

    處理變數名稱藏有空白的情況

--- AutoEvaluator.sh
+++ AutoEvaluator.sh
@@ -5,47 +5,47 @@ QuestionFolders=("Q1_Fractions/" "Q2_Sort/" "Q3_Palindrome/")
 MakefileName=("makefile1" "makefile2" "makefile3")
 MainfileName=("main.cpp1" "main.cpp2" "main.cpp3")
 
-if [ ! -d $OutputfileFolder ]; then
-	mkdir -p $OutputfileFolder
+if [ ! -d "$OutputfileFolder" ]; then
+	mkdir -p "$OutputfileFolder"
 fi
 
-for file in  $(ls $CodeFolder | egrep -i '*.zip|*.rar|*.7z');
+if [ ! -d "$DecompressedFolder" ]; then
+	mkdir -p "$DecompressedFolder"
+fi
+
+for file in  $(ls "$CodeFolder" | egrep -i '*.zip|*.rar|*.7z');
 do
 
-	OutputfilePath=$OutputfileFolder$file.log
-	if [ -f $OutputfilePath ]; then
-		rm -f $OutputfilePath
+	OutputfilePath="$OutputfileFolder""$file".log
+	if [ -f "$OutputfilePath" ]; then
+		rm -f "$OutputfilePath"
 	fi
 
-	FolderContainsQuestions=$DecompressedFolder
+	FolderContainsQuestions="$DecompressedFolder"
 
-	if [ ! -d $DecompressedFolder ]; then
-		mkdir -p $DecompressedFolder
+	if [ "$(ls -A  "$DecompressedFolder")" ]; then		# Folder is not empty
+		rm -rf "$DecompressedFolder"/*
 	fi
 
-	if [ "$(ls -A  $DecompressedFolder)" ]; then		# Folder is not empty
-		rm -rf $DecompressedFolder/*
-	fi
-
-	if [[ $file =~ ".zip" ]]; then
-		unzip $CodeFolder$file -d $DecompressedFolder
+	if [[ "$file" =~ ".zip" ]]; then
+		unzip "$CodeFolder$file" -d "$DecompressedFolder"
 	elif [[ $file =~ ".rar" ]]; then
-		rar x $CodeFolder$file $DecompressedFolder
+		rar x "$CodeFolder$file" "$DecompressedFolder"
 	elif [[ $file =~ ".7z" ]]; then
-		7z x $CodeFolder$file -o$DecompressedFolder
+		7z x "$CodeFolder$file" -o"$DecompressedFolder"
 	fi
 
-	while [ ! -d $FolderContainsQuestions${QuestionFolders[0]} ]		# try to find the folder which really contains QuestionFolders
+	while [ ! -d "${FolderContainsQuestions}""${QuestionFolders[0]}" ]		# try to find the folder which really contains QuestionFolders
 	do
-		FolderContainsQuestions=$FolderContainsQuestions"$(ls $FolderContainsQuestions -A)/"
+		FolderContainsQuestions="$FolderContainsQuestions""$(ls $FolderContainsQuestions -A)/"
 	done
 
 	for ((i=0; i < ${#QuestionFolders[@]}; i++))
 	do
-		cp -f ${MakefileName[$i]} $FolderContainsQuestions${QuestionFolders[$i]}makefile
-		cp -f ${MainfileName[$i]} $FolderContainsQuestions${QuestionFolders[$i]}main.cpp
-		make -C $FolderContainsQuestions${QuestionFolders[$i]}
-		$FolderContainsQuestions${QuestionFolders[$i]}binary.out >> $OutputfilePath
+		cp -f "${MakefileName[$i]}" "$FolderContainsQuestions""${QuestionFolders[$i]}"makefile
+		cp -f "${MainfileName[$i]}" "$FolderContainsQuestions""${QuestionFolders[$i]}"main.cpp
+		make -C "$FolderContainsQuestions""${QuestionFolders[$i]}"
+		"$FolderContainsQuestions""${QuestionFolders[$i]}"binary.out >> "$OutputfilePath"
 	done
 
 done
