71ea7fbe51eca026409f3a473924c67f58a75adb
NaI <fd4124983399@gmail.com>
2020-08-04 13:19:41 +0800

    保留所有解壓縮檔，解決7z創建新目錄的權限問題
    只有在程式初始化過程會把解壓縮目錄全體清空

--- AutoEvaluator.sh
+++ AutoEvaluator.sh
@@ -15,27 +15,27 @@ if [ ! -d "$DecompressedFolder" ]; then
 	mkdir -p "$DecompressedFolder"
 fi
 
+if [ "$(ls -A  "$DecompressedFolder")" ]; then		# Folder is not empty
+	rm -rf "$DecompressedFolder"/*
+fi
+
 for file in  $(ls "$CodeFolder" | egrep -i '*.zip|*.rar|*.7z');
 do
-
 	OutputfilePath="$OutputfileFolder""$file".log
 	if [ -f "$OutputfilePath" ]; then
 		rm -f "$OutputfilePath"
 	fi
 
-	if [ "$(ls -A  "$DecompressedFolder")" ]; then		# Folder is not empty
-		rm -rf "$DecompressedFolder"/*
-	fi
-
 	if [[ "$file" =~ ".zip" ]]; then
-		unzip "$CodeFolder$file" -d "$DecompressedFolder"
+		unzip "$CodeFolder$file" -d "$DecompressedFolder""$file"
 	elif [[ $file =~ ".rar" ]]; then
-		rar x "$CodeFolder$file" "$DecompressedFolder"
+		rar x "$CodeFolder$file" "$DecompressedFolder""$file"
 	elif [[ $file =~ ".7z" ]]; then
-		7z x "$CodeFolder$file" -o"$DecompressedFolder"
+		7z x "$CodeFolder$file" -o"$DecompressedFolder""$file"
 	fi
 
-	FolderContainsQuestions="$(find -type d -name ${QuestionFolders[0]::-1} -printf '%h\n' -quit | sort -u)/"
+	chmod -R 755 "$DecompressedFolder""$file"		# use 7z to create a new folder seems set the permission to 700
+	FolderContainsQuestions="$(find "$DecompressedFolder""$file" -type d -name ${QuestionFolders[0]::-1} -printf '%h\n' -quit | sort -u)/"
 	for ((i=0; i < ${#QuestionFolders[@]}; i++))
 	do
 		cp -f "${MakefileName[$i]}" "$FolderContainsQuestions""${QuestionFolders[$i]}"makefile
